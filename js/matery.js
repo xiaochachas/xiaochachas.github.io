61e7eabc1f438e9cdf065096691990314329e881
unknown <6464654@163.com>
2021-12-22 18:28:13 +0800

    Site updated: 2021-12-22 18:28:13

--- js/matery.js
+++ js/matery.js
@@ -1,3 +1,18 @@
+ function switchNightMode() {
+        var body = document.body;
+        if(body.classList.contains('dark')){
+        document.body.classList.remove('dark');
+        localStorage.setItem('dark','0');
+        $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o");
+        return;
+        } else {
+        document.body.classList.add('dark');
+        localStorage.setItem('dark','1');
+        $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o");
+        return;
+        }
+    }
+
 $(function () {
     /**
      * 添加文章卡片hover效果.
